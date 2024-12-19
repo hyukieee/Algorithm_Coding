0644e5596e735ba4500a7d00d9b0d099d39a5e33
hyukieee <141428745+hyukieee@users.noreply.github.com>
2024-12-19 17:31:11 +0900

    [Bronze V] Title: 시험 성적, Time: 84 ms, Memory: 12324 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    val grade : Int = readln().toInt()
+
+    when(grade){
+       in 90..100 -> println('A')
+       in 80..89 -> println('B')
+       in 70..79 -> println('C')
+       in 60..69 -> println('D')
+       else -> println('F')
+    }
+}
