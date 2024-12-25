0c90a9fc749f346dcce0732200ea6606b9da6ec6
hyukieee <141428745+hyukieee@users.noreply.github.com>
2024-12-25 19:09:49 +0900

    [Silver V] Title: 숫자 카드, Time: 400 ms, Memory: 121096 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+cards = set(map(int, input().split()))
+
+M = int(input())
+queries = list(map(int, input().split()))
+
+result = []
+
+for query in queries:
+    if query in cards:
+        result.append('1')
+    else:
+        result.append('0')
+
+print(' '.join(result))
