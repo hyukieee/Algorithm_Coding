6720bd79fce49df89c4692d3b90ab83e89ecdbb7
hyukieee <141428745+hyukieee@users.noreply.github.com>
2025-01-21 22:55:49 +0900

    [Bronze II] Title: 소수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,18 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+M = int(input())
+N = int(input())
+
+primes = [i for i in range(M, N + 1) if is_prime(i)]
+
+if primes:
+    print(sum(primes))
+    print(min(primes))
+else:
+    print(-1)
